0fd70b093844a833f58cc539b6a2f51e4d9d06ae
Techbuddy-code <kareenapatel3473@gmail.com>
2026-08-14 14:31:54 +0530

    Commit 3 by kareena

--- hello.c
+++ hello.c
@@ -3,5 +3,6 @@
 int main() {
     printf("Hello, World!\n");
     printf("This project uses the C11 language standard version.\n");
+    printf("This project uses the CMake build system.\n");
     return 0;
 }
